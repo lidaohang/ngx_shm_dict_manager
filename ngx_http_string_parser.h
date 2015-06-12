0094ed2636bd9fe75030ecfd1440cc2aeac25eaa
daohang <357732053@126.com>
2015-06-12 15:49:53 +0800

    Create ngx_http_string_parser.h

---
+++ ngx_http_string_parser.h
@@ -0,0 +1,8 @@
+#ifndef _NGX_HTTP_STRING_PARSER_H_INCLUDED_
+#define _NGX_HTTP_STRING_PARSER_H_INCLUDED_
+
+
+
+int ngx_shm_string_parser(ngx_event_t *ev,ngx_socket_t fd,char *b,ssize_t len);
+
+#endif /* _NGX_HTTP_STRING_PARSER_H_INCLUDED_ */
